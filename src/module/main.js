263e628a87092a1810dc148e3a766b5746ab1f05
EgoRaf2001 <105889535+EgoRaf2001@users.noreply.github.com>
2022-06-17 15:51:21 +0400

    Update main.js

--- src/module/main.js
+++ src/module/main.js
@@ -1,40 +1,49 @@
-// 1) login anel
-// 2) refreshic heto logout chanel
-
 const correctCredentials = {
-  userName: "ego",
-  password: "ego",
+    userName: "ego",
+    password: "ego",
 };
 
-function signup() {
-  const username = document.getElementById("username").value;
+function checkCredentials() {
+    const loginData = localStorage.getItem("login");
+    const user = JSON.parse(loginData);
 
-  const pass = document.getElementById("pass").value;
+    if(user?.username === correctCredentials.userName && user?.password === correctCredentials.password) {
+        return true;
+    }
 
-  const user = {
-    username: username,
-    password: pass,
-  };
+    return false;
+}
 
-  if (
-    user.username === correctCredentials.userName &&
-    user.password === correctCredentials.password
-  ) {
-    const json = JSON.stringify(user);
-    localStorage.setItem("login", json);
+function logIn() {
+    const username = document.getElementById("username").value;
+    const pass = document.getElementById("pass").value;
 
-    return;
-  }
+    const user = {
+        username: username,
+        password: pass,
+    };
 
-  console.log("wrong credentials");
+    if (
+        user.username === correctCredentials.userName &&
+        user.password === correctCredentials.password
+    ) {
+        const json = JSON.stringify(user);
+        localStorage.setItem("login", json);
+
+        renderPage();
+
+        return;
+    }
+
+    console.log("wrong credentials");
 }
 
 const form = document.getElementById("form");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  signup();
+    logIn();
 });
 
 //to do list
@@ -43,41 +52,43 @@ const todosList = document.querySelector(".todoslist");
 const todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 function addTodo(e) {
-  e.preventDefault();
-  const text = this.querySelector("[type=text]").value;
-  const todo = {
-    text,
-    done: false,
-  };
-  todos.push(todo);
-  populateList(todos, todosList);
-  localStorage.setItem("todos", JSON.stringify(todos));
-  this.reset();
+    e.preventDefault();
+    const text = this.querySelector("[type=text]").value;
+    const todo = {
+        text,
+        done: false,
+    };
+
+    todos.push(todo);
+    populateList(todos, todosList);
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    this.reset();
 }
 
 function populateList(todos = [], todosList) {
-  todosList.innerHTML = todos
-    .map((todo, i) => {
-      return `
+    todosList.innerHTML = todos
+        .map((todo, i) => {
+            return `
           <li>
           <input type="checkbox" data-index=${i} id="todo${i}"  ${
-        todo.done ? "checked" : ""
-      }/>
+                todo.done ? "checked" : ""
+            }/>
           <label for="todo${i}">${todo.text}</label>
           </li>
         
         `;
-    })
-    .join("");
+        })
+        .join("");
 }
 
 function toggleDone(e) {
-  if (!e.target.matches("input")) return;
-  const target = e.target;
-  const index = target.dataset.index;
-  todos[index].done = !todos[index].done;
-  localStorage.setItem("todos", JSON.stringify(todos));
-  populateList(todos, todosList);
+    if (!e.target.matches("input")) return;
+    const target = e.target;
+    const index = target.dataset.index;
+    todos[index].done = !todos[index].done;
+    localStorage.setItem("todos", JSON.stringify(todos));
+    populateList(todos, todosList);
 }
 
 addTodos.addEventListener("submit", addTodo);
@@ -86,48 +97,42 @@ todosList.addEventListener("click", toggleDone);
 populateList(todos, todosList);
 
 //change log in-log out
-const loginDiv = document.querySelector(".login");
-const hideDiv = document.querySelector(".hide");
-const todosDiv = document.querySelector(".todos");
+const loginDiv = document.querySelector("#loginContainer");
+const todosDiv = document.querySelector("#todosContainer");
 
-function change() {
-  if (loginDiv.style.display === "none") {
-    hideDiv.style.display = "none";
-    todosDiv.style.display = "none";
-    loginDiv.style.display = "block";
-  } else {
-    loginDiv.style.display = "none";
-    hideDiv.style.display = "block";
-    todosDiv.style.display = "block";
-  }
+function isAuthorised() {
+    if (checkCredentials()) {
+        return true;
+    }
+
+    logOut();
+    return false;
 }
 
-const logIn = document.getElementById("logIn");
-
-logIn.addEventListener("click", connect);
-
-
-function connect() {
-  const username = document.getElementById("username").value;
-
-  const pass = document.getElementById("pass").value;
-
-  const user = {
-    username: username,
-    password: pass,
-  };
-  if (
-    user.username === correctCredentials.userName &&
-    user.password === correctCredentials.password
-  ) {
-    change();
-  }
-  return;
+function logOut() {
+    localStorage.clear();
 }
 
-const logOut = document.getElementById("logOut");
-logOut.addEventListener("click", change);
-logOut.addEventListener("click", removeItem);
-function removeItem() {
-  localStorage.removeItem("login");
+function renderPage() {
+    if (isAuthorised()) {
+        loginDiv.classList.add("hide");
+        todosDiv.classList.remove("hide");
+
+        return;
+    }
+
+    loginDiv.classList.remove("hide");
+    todosDiv.classList.add("hide");
 }
+
+const logOutButton = document.getElementById("logOut");
+
+logOutButton.addEventListener("click", function () {
+    logOut()
+    renderPage()
+});
+
+
+window.addEventListener("load", function () {
+    renderPage()
+});
