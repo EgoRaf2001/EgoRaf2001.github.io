ab19a3aff61bb1789933a9767209d85d4ece4a6e
EgoRaf2001 <105889535+EgoRaf2001@users.noreply.github.com>
2022-06-29 13:13:54 +0400

    Add files via upload

--- src/module/theme.js
+++ src/module/theme.js
@@ -1,21 +1,33 @@
 const themTogglerButton = document.getElementById("dark-theme-button");
+let dataTheme = localStorage.getItem("data-theme");
 let browserTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
-  ? "dark"
-  : "light";
+  ? "light"
+  : "dark";
 
-const setTheme = (theme = browserTheme) => {
+const setTheme = function (theme = browserTheme) {
   document.documentElement.setAttribute("data-theme", theme);
 };
 
 const toggleTheme = () => {
   const currentTheme = document.documentElement.getAttribute("data-theme");
-  const theme = currentTheme === "dark" ? "light" : "dark";
+  if (currentTheme === "dark") {
+    localStorage.setItem("data-theme", "light");
+    theme = "light";
+  } else {
+    localStorage.setItem("data-theme", "dark");
+    theme = "dark";
+  }
 
   setTheme(theme);
 };
 
 themTogglerButton.addEventListener("click", () => {
   toggleTheme();
+});
+window.addEventListener("load", function () {
+  if (dataTheme) {
+    setTheme(dataTheme);
+  }
 });
 
 window
